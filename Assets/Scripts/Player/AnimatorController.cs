b3d60d237535a923f71e15601627dd20d1b5d0f8
jiangbo <jiangb2o@foxmail.com>
2024-09-26 21:05:35 +0800

    Animator控制

---
+++ Assets/Scripts/Player/AnimatorController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    public Button attackButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        attackButton.onClick.AddListener(Attack);
+    }
+
+    private void Attack()
+    {
+        //animator.SetBool("isAttack", true);
+        animator.SetTrigger("T_isAttack");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
