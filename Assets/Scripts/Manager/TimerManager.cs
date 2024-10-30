b25f93b8a018f52eb69224f39e384c2aab6933c3
jiangbo <jiangb2o@foxmail.com>
2024-10-30 21:08:35 +0800

    删除冗余的List<Timer>

--- Assets/Scripts/Manager/TimerManager.cs
+++ Assets/Scripts/Manager/TimerManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TimerManager : MonoSingleton<TimerManager>
 {
-    private List<Timer> timers;
     private Dictionary<string, Timer> timerDictionary;
     
     // Start is called before the first frame update
@@ -12,30 +11,28 @@ public class TimerManager : MonoSingleton<TimerManager>
     {
         base.Awake();
         // 要在Awake初始化, 如果在Start初始化, 可能在Start之前调用方法导致未初始化错误
-        timers = new List<Timer>();
         timerDictionary = new Dictionary<string, Timer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Timer timer in timers)
+        foreach (var item in timerDictionary)
         {
-            timer.OnUpdate(Time.deltaTime);
+            // Debug.Log("Timer:" + item.Key + " : " + item.Value.leftTime + " s");
+            item.Value.OnUpdate(Time.deltaTime);
         }
     }
 
     public void AddTimer(string str, Timer timer)
     {
-        // 重置时间
         if (timerDictionary.ContainsKey(str))
         {
-            timerDictionary[str].Reset();
+            Debug.Log("[TimerManager]:" + str + " is already registered");
         }
         else
         {
             timerDictionary.Add(str, timer);
-            timers.Add(timer);
         }
     }
 
@@ -44,10 +41,10 @@ public class TimerManager : MonoSingleton<TimerManager>
         Timer timer = timerDictionary[str];
         if (timer != null)
         {
-            timers.Remove(timer);
             timerDictionary.Remove(str);
         }
     }
 
     public void ResetTimer(string str) => timerDictionary[str]?.Reset();
+    
 }
