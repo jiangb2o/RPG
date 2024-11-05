9b5a980ef1993b4f267f6db4d56fba7094f3019d
jiangbo <jiangb2o@foxmail.com>
2024-11-05 17:46:52 +0800

    修改武器基类, 可以使玩家和敌人武器都继承此基类

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -1,33 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Weapon : MonoBehaviour
 {
-    public PlayerProperty playerProperty;
+    public bool ownerIsPlayer;
+    public GameObject owner;
+    public Property ownerProperty;
     public GameObject textPopupPrefab;
     public virtual void Attack() { }
 
     protected virtual void Start()
     {
-        playerProperty = GameObject.FindGameObjectWithTag(Tag.PLAYER).GetComponent<PlayerProperty>();
+        owner = GameObject.FindWithTag(Tag.PLAYER);
+        ownerProperty = owner.GetComponent<PlayerProperty>();
+        ownerIsPlayer = true;
     }
 
     public void CalculateDamage(GameObject target)
     {
         GameObject textPopupGameObject = Instantiate(textPopupPrefab, target.transform);
+        PopupText popupText = textPopupGameObject.GetComponent<PopupText>();
+        popupText.isEnemy = ownerIsPlayer;
         
         float damage = 0;
-        if (Utils.GetRandomRate() > playerProperty.criticalRate.value)
+        if (Utils.GetRandomRate() > ownerProperty.criticalRate.value)
         {
-            damage = playerProperty.attack.value;
+            damage = ownerProperty.attack.value;
         }
         else
         {
-            textPopupGameObject.GetComponent<PopupText>().isCritical = true;
-            damage = playerProperty.attack.value * (1 + playerProperty.criticalDamage.value / 100f);
+            popupText.isCritical = true;
+            damage = ownerProperty.attack.value * (1 + ownerProperty.criticalDamage.value / 100f);
         }
-        textPopupGameObject.GetComponent<PopupText>().damage = damage;
-        target.GetComponent<EnemyAttacked>().TakeDamage(damage);
+        popupText.damage = damage;
+
+        if (ownerIsPlayer)
+        {
+            target.GetComponent<EnemyAttacked>().TakeDamage(damage);
+        }
+        else
+        {
+            target.GetComponent<PlayerProperty>().TakeDamage(damage);
+        }
     }
 }
